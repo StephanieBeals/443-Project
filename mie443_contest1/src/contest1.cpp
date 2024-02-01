449b1cbe726b1dbfb84dbf5c091523898b8f08c5
vboxuser <vboxuser@Ubuntu16.myguest.virtualbox.org>
2024-02-01 11:39:20 -0500

    Starting skeleton for state funcs

--- mie443_contest1/src/contest1.cpp
+++ mie443_contest1/src/contest1.cpp
@@ -26,6 +26,11 @@ uint8_t bumper[3]={kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent:
 float minLaserDist=std::numeric_limits<float>::infinity();
 int32_t nLasers=0, desiredNLasers=0, desiredAngle=15;
 
+//Used in laser callback
+bool dynMem=false;
+float* laserVals;
+int minLaserIdx
+
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
 	//fill with your code
@@ -37,18 +42,38 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 	minLaserDist = std::numeric_limits<float>::infinity();
     nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
     desiredNLasers = desiredAngle*M_PI / (180*msg->angle_increment);
+    if (!dynMem){
+        laserVals= new float(desiredNLasers+1); //+1 just in case?
+        dynMem=true;
+    }
+
 
     //ROS_INFO("Size of laser scan array: %i and size of offset: %i", nLasers, desiredNLasers);
+    int laserValIndex=0;
     if (desiredAngle * M_PI / 180 < msg->angle_max && -desiredAngle * M_PI / 180 > msg->angle_min) {
         for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx){
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+            if (minLaserDist>msg->ranges[laser_idx]){
+                minLaserDist = msg->ranges[laser_idx];
+                minLaserIdx=laser_idx;
+            }
+            laserVals[laserValIndex]=msg->ranges[laser_idx];
+            laserValIndex++;
         }
     }
     else {
         for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx) {
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+            //minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
+            if (minLaserDist>msg->ranges[laser_idx]){
+                minLaserDist = msg->ranges[laser_idx];
+                minLaserIdx=laser_idx;
+            }
+            laserVals[laserValIndex]=msg->[laser_idx];
+            laserValIndex++;
         }
     }
+
+    
+
     ROS_INFO("Min dist: %f", minLaserDist);
 
 
@@ -62,8 +87,13 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 }
 
 void displaceYaw(int yawDispDeg);
+void navLogic();
+void bumperFailsafe();
+void orientToNormal();
+float idxToAng(int idx);
+bool timeout(uint64_t limit, std::chrono::time_point<std::chrono::system_clock> startPt);
 
-
+bool firstRun=true; //Global variable for if you want something to run only once
 
 int main(int argc, char **argv)
 {
@@ -84,37 +114,29 @@ int main(int argc, char **argv)
     std::chrono::time_point<std::chrono::system_clock> start;
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
+    uint8_t state=0;
 
     int state=0;
+    float distParam=0.8;
 
     while(ros::ok() && secondsElapsed <= 480) {
         ros::spinOnce();
         auto now =  std::chrono::system_clock::now();
         auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(now-start).count();
-        ROS_INFO("Turn start: %lu", millis);
-        //displaceYaw(90);
-        float yawDispRad=DEG2RAD(95);
-        float angularVel=M_PI/6; //30 deg per sec
-        float timeToTurn=yawDispRad/angularVel;
-        std::chrono::time_point<std::chrono::system_clock> start;
-        start = std::chrono::system_clock::now();
-    //ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
-        ros::Publisher yaw_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
-        geometry_msgs::Twist vel;
-        
-        auto yawStart =  std::chrono::system_clock::now();
-        uint64_t millisElapsed=0;
-        while(ros::ok() && millisElapsed<=timeToTurn*1000){
-            ros::spinOnce();
-            vel.angular.z=angularVel;
-            vel.linear.x=linear;
-            yaw_pub.publish(vel);
+        ROS_INFO("Spin start: %lu", millis);
 
-            millisElapsed=std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-yawStart).count();
+        switch(state){
+            case 0:
+            navLogic();
+            break;
+
+            case 1:
+            orientToNormal();
+            break;
+
         }
-        now =  std::chrono::system_clock::now();
-        millis = std::chrono::duration_cast<std::chrono::milliseconds>(now-start).count();
-        ROS_INFO("Turn end: %lu", millis);
+
+        
 
         /*
         if(minLaserDist>0.75 && minLaserDist!=std::numeric_limits<float>::infinity()){
@@ -136,15 +158,93 @@ int main(int argc, char **argv)
             }
         }
         */
+
+        now =  std::chrono::system_clock::now();
+        millis = std::chrono::duration_cast<std::chrono::milliseconds>(now-start).count();
+        ROS_INFO("Spin end: %lu", millis);
+
+        vel.angular.z = angular;
+        vel.linear.x = linear;
+        vel_pub.publish(vel);
         secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
         loop_rate.sleep(); //Why is this here?
     }
+    delete laserVals;
 
     return 0;
 }
-//Use a callback instead???
+//navLogic, lookAround, orientToNormal, decideDirection, moveToPt, navPastObject, bumperFailsafe
+//Corner detection
+//Left clear, right clear as supporting funcs
+void navLogic(){
+    //navLogic decides which states to enter, reactive states are the first few if statements in order of priority, proactive later
+    //Last state is default,move in a straight line forward
+    float distParam=0.8
+    if (bumperPressed){ 
+        linear=0;
+        angular=0;
+        state=1;
+    } else if (minLaserDist<distParam || minLaserDist==std::numeric_limits<float>::infinity()){
+        linear=0;
+        state=2;
+    } else if (doLook){
+        state=9;
+    } else {
+        linear=0.4;
+        angular=0.0;
+        state=0;
+    }
+    return;
+}
+
+
+bool decideDirection(){
+    orientToNormal(); //Need to add this function in
+    displaceYaw(90);
+    float minDist1=minLaserDist;
+    displaceYaw(-180);
+    float minDist2=minLaserDist;
+    if (minDist1>minDist2) displaceYaw(90);
+    else displaceYaw(270);
+    return;
+}
+
+void orientToNormal(){
+    
+    
+    /*
+    distGoal=minLaserDist+0.01; //Tolerance
+    displaceYaw(-30);
+    vel.angular.z=0.2;
+    normDist=getNormDist();
+    std::chrono::time_point<std::chrono::system_clock> startPt;
+    while(getNormDist>distGoal || !timeout(4000, startPt)){
+    }
+    */
+    getNormalDirection();
+    angular=+/-angularVel;
+    if(facingNormal){
+        state=0;
+    } else {return;}
+    
+}
+
+bool timeout(uint64_t limit, std::chrono::time_point<std::chrono::system_clock> startPt){ //Non-blocking timer
+    //Create a time point at when your timer starts and pass it in (see 'now' or 'start' for examples)
+    //Returns true if the limit has passed since the timer start was initialized
+    //Runs in milliseconds
+    auto now = std::chrono::system_clock::now();
+    if (std::chrono::duration_cast<std::chrono::milliseconds>(now-startPt).count()<limit)return false;
+    else return true;
+}
+
+float idxToAng(int idx){
+    idx=idx-desiredNLasers/2;
+    return idx*angle_increment;
+}
+
 /*
-void displaceYaw(int yawDispDeg){ 
+void displaceYaw(int yawDispDeg){  //Deprecated, do not use
     //Need some code handling negative or greater than 360 case.
     float yawDispRad=DEG2RAD(yawDispDeg);
     float angularVel=M_PI/6; //30 deg per sec
@@ -170,32 +270,3 @@ void displaceYaw(int yawDispDeg){
 
     return;
 } */
-
-bool decideDirection(){
-    orientToNormal(); //Need to add this function in
-    displaceYaw(90);
-    float minDist1=minLaserDist;
-    displaceYaw(-180);
-    float minDist2=minLaserDist;
-    if (minDist1>minDist2) displaceYaw(90);
-    else displaceYaw(270);
-    return;
-}
-
-void orientToNormal(){
-    distGoal=minLaserDist+0.01; //Tolerance
-    displaceYaw(-30);
-    vel.angular.z=0.2;
-    normDist=getNormDist();
-    std::chrono::time_point<std::chrono::system_clock> startPt;
-    while(getNormDist>distGoal || !timeout(4000, startPt)){
-    }
-    
-}
-
-bool timeout(uint64_t limit, std::chrono::time_point<std::chrono::system_clock> startPt){ //Non-blocking timer
-    auto now = std::chrono::system_clock::now();
-    if (std::chrono::duration_cast<std::chrono::milliseconds>(now-startPt).count()<limit)return false;
-    else return true;
-    
-}
