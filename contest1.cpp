b2a122dfafad52637517353daf3834aed42407f9
human-bunny <134898146+human-bunny@users.noreply.github.com>
2024-02-05 22:24:36 -0500

    Update contest1.cpp
    test check

--- contest1.cpp
+++ contest1.cpp
@@ -3,129 +3,289 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
+#include <eStop.h>
+
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_datatypes.h>
+
+#include <random>
+
+#include <cmath>
+#include <iostream>
 #include <chrono>
 
-float angular = 0.0;
-float linear = 0.0;
-float posX = 0.0, posY = 0.0, yaw = 0.0;
-
-#define N_BUMPER (3)
-#define RAD2DEG(rad) ((rad) * 180. / M_PI)
-#define DEG2RAD(deg) ((deg) * M_PI / 180.)
-
-uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
-
-float minLaserDist = std::numeric_limits<float>::infinity();
-int32_t nLasers = 0, desiredNLasers = 0, desiredAngle = 5;
-
-void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
+enum Bumper
 {
-    posX = msg->pose.pose.position.x;
-    posY = msg->pose.pose.position.y;
-    yaw = tf::getYaw(msg->pose.pose.orientation);
+	Left = 0,
+	Center = 1,
+	Right = 2,
+};
+
+#define LIN_SPEED 0.25
+
+using namespace std;
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::bernoulli_distribution bernoulli(0.7);
+
+bool turnLeft = true;
+
+Bumper whichBumperHit = Center;
+
+// CALLBACK VARIABLES
+bool bumperLeft = false, bumperCenter = false, bumperRight = false;
+bool isBumperHit = false;
+bool spin = false;
+
+// Odometry
+bool isTurning = false;
+double posX, posX_Init = 0.0, posX_Spin = 0.0;
+double posY, posY_Init = 0.0, posY_Spin = 0.0; 
+double yaw, yawInitial, yawDesired;
+double pi = 3.1416;
+
+// Laser Scan
+double laserRange = 10;
+int laserSize = 0, laserOffset = 0, desiredAngle = 50;
+
+
+
+void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+{
+	if (msg->bumper == 0)
+	{
+		bumperLeft = !bumperLeft;
+	}
+	else if (msg->bumper == 1)
+	{
+		bumperCenter = !bumperCenter;
+	}
+	else if (msg->bumper == 2)
+	{
+		bumperRight = !bumperRight;
+	}
 }
 
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    minLaserDist = std::numeric_limits<float>::infinity();
-    nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
-    desiredNLasers = desiredAngle * M_PI / (180 * msg->angle_increment);
+	laserSize = (msg->angle_max - msg->angle_min)/msg->angle_increment;
+	laserOffset = desiredAngle * pi / (180.0 * msg->angle_increment);
+	laserRange = 11;
 
-    if (desiredAngle * M_PI / 180 < msg->angle_max && -desiredAngle * M_PI / 180 > msg->angle_min)
-    {
-        for (uint32_t laser_idx = nLasers / 2 - desiredNLasers; laser_idx < nLasers / 2 + desiredNLasers; ++laser_idx)
-        {
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
-        }
-    }
-    else
-    {
-        for (uint32_t laser_idx = 0; laser_idx < nLasers; ++laser_idx)
-        {
-            minLaserDist = std::min(minLaserDist, msg->ranges[laser_idx]);
-        }
-    }
+	if (desiredAngle * pi / 180.0 < msg->angle_max && -desiredAngle * pi / 180.0 > msg->angle_min)
+	{
+		for (int i = laserSize/2 - laserOffset; i < laserSize/2 + laserOffset; ++i)
+		{
+			if (laserRange > msg->ranges[i])
+			{
+				laserRange = msg->ranges[i];
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < laserSize; ++i)
+		{
+			if (laserRange > msg->ranges[i])
+			{
+				laserRange = msg->ranges[i];
+			}
+		}
+	}
+
+	if (laserRange == 11)
+	{
+		laserRange = 0;
+	}
+
+	ROS_INFO("Distance Reading: %lf", laserRange);
+	//ROS_INFO("Size of laser scan array: %i and size of offset: %i", laserSize, laserOffset);
 }
 
-void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr &msg)
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
-    bumper[msg->bumper] = msg->state;
+	posX = msg->pose.pose.position.x;
+	posY = msg->pose.pose.position.y;
+	yaw = tf::getYaw(msg->pose.pose.orientation);
+
+	if (isTurning && std::abs(yaw - yawInitial) > pi / 2.5)
+	{
+		isTurning = false;
+		turnLeft = bernoulli(gen) ? true : false;
+	}
+
+	// ROS_INFO("YAW_DIFF: %lf isTurning: %d", std::abs(yaw - yawInitial), isTurning);
+	ROS_INFO("YAW: %lf", yaw);
+}
+
+bool isBumperPressed()
+{
+	return bumperRight || bumperCenter || bumperLeft;
+}
+
+double distFromInit()
+{
+	std::sqrt(std::pow(posX - posX_Init, 2) + std::pow(posY - posY_Init, 2));
+}
+
+double distFromLastSpin()
+{
+	std::sqrt(std::pow(posX - posX_Spin, 2) + std::pow(posY - posY_Spin, 2));
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "robot_mapper");
-    ros::NodeHandle nh;
+	ros::init(argc, argv, "image_listener");
+	ros::NodeHandle nh;
+	teleController eStop;
 
-    ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
-    ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
-    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
-    ros::Subscriber odom = nh.subscribe("odom", 1, &odomCallback);
+	// Subscritpions
+	ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
+	ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
+	ros::Subscriber odom = nh.subscribe("/odom", 1, odomCallback);
 
-    ros::Rate loop_rate(10);
-    geometry_msgs::Twist vel;
+	// Publishers
+	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
-    // contest count down timer
-    std::chrono::time_point<std::chrono::system_clock> start;
-    start = std::chrono::system_clock::now();
-    uint64_t secondsElapsed = 0;
+	geometry_msgs::Twist vel;
+	
+	double linear = 0.0;
+	double angular = 0.0;
 
-    while (ros::ok() && secondsElapsed <= 480)
-    {
-        ros::spinOnce();
+	std::chrono::time_point<std::chrono::system_clock> start;
+	start = std::chrono::system_clock::now(); /* start timer */
+    	uint64_t secondsElapsed = 0; // the timer just started, so we know it is less than 480, no need to check.
+	while(ros::ok() && secondsElapsed <= 480)
+	{
+		// UPDATE VALUES IN CALLBACK SUBSCRIPTION
+		ros::spinOnce();	
 
-        // Check if any of the bumpers were pressed.
-        bool any_bumper_pressed = false;
-        for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx)
-        {
-            any_bumper_pressed |= (bumper[b_idx] == kobuki_msgs::BumperEvent::PRESSED);
-        }
+		//.....**E-STOP DO NOT TOUCH**.......
+		eStop.block();
+		//...................................
 
-        // Control logic based on the robot's position, orientation, laser scan, and bumper status.
-        if (posX < 0.5 && yaw < M_PI / 12 && !any_bumper_pressed && minLaserDist > 0.7)
-        {
-            angular = 0.0;
-            linear = 0.2;
-        }
-        else if (yaw < M_PI / 2 && posX > 0.5 && !any_bumper_pressed && minLaserDist > 0.5)
-        {
-            angular = M_PI / 6;
-            linear = 0.0;
-        }
-        else if (minLaserDist > 1. && !any_bumper_pressed)
-        {
-            linear = 0.1;
-            if (yaw < 17 / 36 * M_PI || posX > 0.6)
-            {
-                angular = M_PI / 12.;
-            }
-            else if (yaw < 19 / 36 * M_PI || posX < 0.4)
-            {
-                angular = -M_PI / 12.;
-            }
-            else
-            {
-                angular = 0;
-            }
-        }
-        else
-        {
-            angular = 0.0;
-            linear = 0.0;
-        }
+		angular = 0.0;
+		linear = LIN_SPEED;
 
-        vel.angular.z = angular;
-        vel.linear.x = linear;
-        vel_pub.publish(vel);
+		if (!isBumperPressed())
+		{
+			if (laserRange < 0.5 && !isTurning)
+			{
+				yawInitial = yaw;
+				isTurning = true;
+			}
 
-        // Update elapsed time
-        auto end = std::chrono::system_clock::now();
-        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+			if (laserRange > 0.5 && laserRange < 0.7)
+			{
+				linear = 0.8 * LIN_SPEED;	
+			}
 
-        loop_rate.sleep();
-    }
+			double dist = distFromLastSpin();
+			if (!spin && dist > 0.6)
+			{
+				ROS_INFO("yawDesired: %lf, yaw: %lf", yawDesired, yaw); 
+				if (yaw < 0)
+				{
+					yawDesired = yaw + 2*pi;
+				}
+				else
+				{
+					yawDesired = yaw;
+				}
 
-    return 0;
+				//yawDesired -= pi/4;
+				if (yawDesired < 0)
+				{
+					yawDesired += 2*pi;
+				}
+				spin = true;
+			}
+		}
+		else
+		{
+			posX_Init = posX;
+			posY_Init = posY;
+			isBumperHit = true;
+
+			if (bumperRight)
+			{
+				whichBumperHit = Bumper::Right;
+			}
+			else if (bumperLeft)
+			{
+				whichBumperHit = Bumper::Left;
+			}
+			else
+			{
+				whichBumperHit = Bumper::Center;
+			}
+			
+		}
+
+		if (isBumperHit)
+		{
+			if (distFromInit() < 0.2)
+			{
+				switch(whichBumperHit)
+				{
+					case Bumper::Right:
+						angular = pi/6;
+						break;
+					case Bumper::Left:
+						angular = -pi/6;
+						break;
+					default:
+						angular = turnLeft ? pi/6 : -pi/6;
+				}
+
+				linear = -LIN_SPEED;
+			}
+			else
+			{
+				isBumperHit = false;
+			}
+		}
+
+		if (isTurning && turnLeft)
+		{
+			angular = pi/6;
+			linear = 0.0;	
+		}
+		else if (isTurning && !turnLeft)
+		{
+			angular = -pi/6;
+			linear = 0.0;
+		}
+
+		if (spin)
+		{
+			angular = pi/6;
+			linear = 0.0;
+			
+			// Stop Condition
+			float yawWack = yaw;
+			if (yawWack < 0)
+			{
+				yawWack = 2*pi + yawWack;
+			}
+			//ROS_INFO("lower bound: %lf, yaw: %lf, upper bound: %lf", yawDesired-0.5, yawWack, yawDesired);
+			if (yawWack > yawDesired-pi/18 && yawWack < yawDesired)
+			{
+				spin = false;
+				posX_Spin = posX;
+				posY_Spin = posY;
+			}
+		}
+		
+  		vel.angular.z = angular;
+  		vel.linear.x = linear;
+
+  		vel_pub.publish(vel);
+
+		// The last thing to do is to update the timer.
+		secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+	}
+
+	return 0;
 }
